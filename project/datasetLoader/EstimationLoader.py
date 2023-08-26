caa5ff185c0a7fb39b4b4345f4c0b34386cbc08f
Seif Wael <seifeldin7666@gmail.com>
2023-08-27 00:56:01 +0300

    Changed length

--- project/datasetLoader/EstimationLoader.py
+++ project/datasetLoader/EstimationLoader.py
@@ -62,7 +62,7 @@ class EstimationLoader:
             image= self.__loadImage(filename+'.JPG')
 
         annotations= self.__dataset.loc[self.__dataset['file']==filename]
-        length= 13#len(annotations)
+        length= 20#len(annotations)
         bboxes= torch.zeros((length,5))
         sizes= torch.zeros((length,1))
         for i in range(len(annotations)):
