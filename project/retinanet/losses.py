8217c3f9479b5716c4d5029e01808dc7894cb6d3
Seif Wael <seifeldin7666@gmail.com>
2023-09-07 20:54:36 +0300

    Changed Relative error again

--- project/retinanet/losses.py
+++ project/retinanet/losses.py
@@ -161,7 +161,8 @@ class FocalLoss(nn.Module):
                 negative_indices = 1 + (~positive_indices)
 
                 regression_diff = torch.abs(targets - regression[positive_indices, :4])
-                size_diff= ((assigned_annotations[:,-1]- regression[positive_indices,-1])**2)/(assigned_annotations[:,-1]**2)
+                size_diff= (assigned_annotations[:,-1]- regression[positive_indices,-1])/(assigned_annotations[:,-1])
+                size_diff=torch.sqrt(size_diff**2)
                 sizes_losses.append(size_diff.mean())
 
 
