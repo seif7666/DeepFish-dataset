4d023ca2211ac80f8457426e812a7220508f399c
Seif Wael <seifeldin7666@gmail.com>
2023-09-09 03:01:31 +0300

    Visualizing is better

--- project/visualize.py
+++ project/visualize.py
@@ -8,25 +8,29 @@ from retinanet.PipelineModel import PipelineModel
 def draw_caption(image, box, caption):
 
 		b = np.array(box).astype(int)
-		cv2.putText(image, caption, (b[0], b[1] - 10), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 2)
-		cv2.putText(image, caption, (b[0], b[1] - 10), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
+		cv2.putText(image, caption, (((b[0]+b[0])//2), (2*b[1]+b[3])//3), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 2)
+		cv2.putText(image, caption, (((b[0]+b[0])//2), (2*b[1]+b[3])//3), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
 
 def evaluate(model:torch.nn.Module,data:dict,unnormalize:UnNormalizer,classes:np.ndarray):
     # model.cuda()
     scores, classification, transformed_anchors = model(torch.unsqueeze(data['img'].cuda(),dim=0).float())
     print('Evaluation complete!')
-    idxs = np.where(scores.cpu()>0.85)
+    idxs = np.where(scores.cpu()>0.6)
+    print(scores)
     img = np.array(255 * unnormalize(data['img'][:, :, :])).copy()
     img[img<0] = 0
     img[img>255] = 255
     img = np.transpose(img, (1, 2, 0)).astype(np.uint8)
-    cv2.imshow('Before',img)
+    # cv2.imshow('Before',img)
     img = cv2.cvtColor(img.astype(np.uint8), cv2.COLOR_BGR2RGB)
     # img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-
+    pred_img,target_img= img.copy(),img.copy()
     
     # print(transformed_anchors)
     for j in range(idxs[0].shape[0]):
+        # print(classification[idxs[0][j]] )
+        if classification[idxs[0][j]] != 5:
+              continue
         bbox = transformed_anchors[idxs[0][j], :]
         x1 = int(bbox[0])
         y1 = int(bbox[1])
@@ -35,17 +39,34 @@ def evaluate(model:torch.nn.Module,data:dict,unnormalize:UnNormalizer,classes:np
         print(f'Length is {bbox[4]}')
         # print(scores[idxs[0][j]].item())
         # label_name = classes[int(classification[idxs[0][j]])]+" "+str(scores[idxs[0][j]].item())
-        # draw_caption(img, (x1, y1, x2, y2), label_name)
 
-        cv2.rectangle(img, (x1, y1), (x2, y2), color=(0,100+j*10 , 255//(j+1)), thickness=2)
+        label_name = str(int(bbox[4].item()))
         # print(label_name)
-
+        draw_caption(pred_img, (x1, y1, x2, y2), label_name)
+        cv2.rectangle(pred_img, (x1, y1), (x2, y2), color=(0,100+j*10 , 255//(j+1)), thickness=2)
+        # print(label_name)
+    for j in range(data['number']):
+        bbox = data['annot'][j]
+        x1 = int(bbox[0])
+        y1 = int(bbox[1])
+        x2 = int(bbox[2])
+        y2 = int(bbox[3])
+        # print(f'Length is {bbox[4]}')
+        # print(scores[idxs[0][j]].item())
+        label_name = str(int(bbox[5].item()))
+        print(label_name)
+        draw_caption(target_img, (x1, y1, x2, y2), label_name)
+        cv2.rectangle(target_img, (x1, y1), (x2, y2), color=(0,100+j*10 , 255//(j+1)), thickness=2)
+        
+          
     number= data['number']
     lengths= data['annot'][:number,5].to(torch.int)
     print(lengths)
     # for i in lengths:
         #   print(classes[i])
-    cv2.imshow('img', img)
+    cv2.imshow('Predictions', pred_img)
+    cv2.imshow('Target', target_img)
+
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
@@ -87,6 +108,6 @@ def test():
 
       
 if __name__=='__main__':
-    # main()
-    test()
+    main()
+    # test()
     pass
